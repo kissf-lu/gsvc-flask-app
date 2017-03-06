b146c8eb22af8ee907a1b28df75a5df472dc872a
kissf <chois_lu@outlook.com>
2017-03-06 10:40:26 +0800

    addvsim

---
+++ app/api_1_0/api_functions/SqlPack/SqlLinkInfo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+DataApiFuncSqlLink = {
+    "DeleManuleVsimSrc": {"db": "config_DevAmz", "database": "gsvcdatabase", "sheet": "vsim_manual_infor"},
+    "InsertManuleVsimSrc": {
+        "insertDB": {"db": "config_DevAmz", "database": "gsvcdatabase", "sheet": "vsim_manual_infor"},
+        "getUpdateData": {"db": "config_N", "database": "glocalme_css"}
+    },
+    "updateManuleVsimSrc": {"db": "config_DevAmz", "database": "gsvcdatabase", "sheet": "vsim_manual_infor"}
+}
