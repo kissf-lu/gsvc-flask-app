9ad00415a28732826dee5e2f914d313797f57180
kissf <chois_lu@outlook.com>
2017-03-07 15:19:19 +0800

    addvsim

--- app/api_1_0/api_functions/updateSqlPack/deleteModel.py
+++ app/api_1_0/api_functions/updateSqlPack/deleteModel.py
@@ -1,11 +1,11 @@
-#coding=utf-8
-#!/usr/bin/env python
-'''
+# -*- coding: utf-8 -*-
+
+
+"""
 该脚本用于手工维护表批量删除：只删除非空值，剔除空值
 Created on 2016年6月28日
-
 @author: lujian
-'''
+"""
 
 import mysql.connector
 from ..SqlPack.SQLModel import (get_DataBaseConn)
@@ -17,29 +17,31 @@ def popBlankValue(popData):
     :param popData:
     :return:
     """
-    if popData==[]:
+    if not popData:
         return False
     for i in range(len(popData)):
-        popDic=[]
+        popDic = []
         for dic in popData[i].keys():
-            if popData[i][dic]=="":
+            if popData[i][dic] == "":
                 popDic.append(dic)
         for dic in popDic:
             popData[i].pop(dic)
 
     return popData
 
-def deleteManuleVsim(SqlInfo,DicData):
+
+def deleteManuleVsim(SqlInfo, DicData):
     """
 
+    :param SqlInfo:
     :param DicData:
     :return:
     """
     errInfo = ''
     imsiData = DicData
-    strimsi=""
+    strimsi = ""
     for i in range(len(imsiData)):
-        if i != len(imsiData) -1:
+        if i != (len(imsiData) - 1):
             strimsi = strimsi + "'" + imsiData[i]['imsi'] + "',"
         else:
             strimsi = strimsi + "'" + imsiData[i]['imsi'] + "'"
@@ -60,7 +62,8 @@ def deleteManuleVsim(SqlInfo,DicData):
         return errInfo
     return errInfo
 
-def deleteManualModel(SqlInfo,arrayDicData):
+
+def deleteManualModel(SqlInfo, arrayDicData):
     """
 
     :param SqlInfo:
@@ -68,10 +71,12 @@ def deleteManualModel(SqlInfo,arrayDicData):
     :return:
     """
     deletData = arrayDicData
-    if deletData != []:
+    if deletData:
         popdata = popBlankValue(deletData)
-        DeleteActionInfo = deleteManuleVsim(SqlInfo=SqlInfo,DicData=popdata)
+        DeleteActionInfo = deleteManuleVsim(SqlInfo=SqlInfo, DicData=popdata)
+
         return DeleteActionInfo
     else:
         DeleteActionInfo = "删除数据为空！"
+
         return DeleteActionInfo
