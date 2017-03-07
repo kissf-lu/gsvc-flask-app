6064b8a4894599b5ed796015b84d1e8b139bfd6a
kissf <chois_lu@outlook.com>
2017-03-07 16:57:02 +0800

    update insertmodel of api_0_1

--- app/api_1_0/api_functions/updateSqlPack/insertoneColModle.py
+++ app/api_1_0/api_functions/updateSqlPack/insertoneColModle.py
@@ -1,18 +1,19 @@
-#coding=utf-8
-#!/usr/bin/env python
-'''
+# -*- coding: utf-8 -*-
+
+
+"""==============================================
 该脚本用于手工维护表插入新imsi：只更新非空值
 Created on 2016年6月28日
-
+=================================================
 @author: lujian
-'''
+================================================="""
+
 
 import os
 import sys
 import csv
 import mysql.connector
 from ..SqlPack.SQLModel import (qureResultAsJson, get_DataBaseConn)
-
 
 
 def cur_file_dir():
@@ -33,11 +34,11 @@ def getDataFromCsv():
 
     try:
         with open(full_dir_file, 'r') as csvfile:
-            reader = csv.DictReader(csvfile,restkey=None, restval=None)#, dialect='excel'
+            reader = csv.DictReader(csvfile, restkey=None, restval=None)
             for row in reader:
                 reader_Inser.append(row)
             print(reader_Inser[0:1])
-            print("Insert Data Num：",len(reader_Inser))
+            print("Insert Data Num：", len(reader_Inser))
             return reader_Inser
     except:
         """此处有异常返回异常结果，使程序继续执行至其余代码
@@ -47,12 +48,12 @@ def getDataFromCsv():
 
 
 def get_key_value_str(data, key):
-    """
+    """========================================================
     返回："'a1','a2', ..."类型的字符串 方便 sql in ('')类数据查询
     :param data: 包含key数据的原始数据
     :param key: 数据键
     :return: 返回data数据对应的key值字符串
-    """
+    ============================================================"""
     strResult = ""
     for i in range(len(data)):
         if i != 0:
@@ -63,11 +64,11 @@ def get_key_value_str(data, key):
 
 
 def add_dickey(Json_Result, key_value):
-    """
+    """============================================
     :param Json_Result:
     :param key_value:
     :return:
-    """
+    ================================================"""
     if Json_Result == []:
         # print('函数传入转换字典为空[]，无转换数据！')
         return []
@@ -75,37 +76,41 @@ def add_dickey(Json_Result, key_value):
         try:
             Json_Result[i].update(key_value)
         except KeyError as keyerr:
-            print('KeyError:',keyerr)
+            print('KeyError:', keyerr)
     return Json_Result
 
 
 def JsonDataConvert(Json_Result):
-    """
-    """
-    if Json_Result ==[]:
+    """==========================================
+
+    :param Json_Result: list type
+    :return:
+    =============================================="""
+    if not Json_Result:
         print('JsonDataConvert No Data Convert!!!')
         return []
     else:
-        for i in range(len((Json_Result))):
+        for i in range(len(Json_Result)):
             try:
                 for key in Json_Result[i].keys():
-                    if type(Json_Result[i][key]) is bytearray:#byte类数据转换
-                        Json_Result[i][key] = Json_Result[i][key].decode()#输出后进行输出结果整形
+                    if type(Json_Result[i][key]) is bytearray:              # byte类数据转换
+                        Json_Result[i][key] = Json_Result[i][key].decode()  # 输出后进行输出结果整形
 
             except KeyError as Flowerkeyerr:
-                print('Flowerkeyerr:',Flowerkeyerr)
+                print('Flowerkeyerr:', Flowerkeyerr)
 
         return Json_Result
 
 
-def fetchUpdateDataFromSys(sql_info,update_imsi):
-    """
+def fetchUpdateDataFromSys(sql_info, update_imsi):
+    """======================================================
 
+    :param sql_info:
     :param update_imsi:
     :return:
-    """
+    =========================================================="""
 
-    N_DataStr=(
+    N_DataStr = (
         "SELECT "
         "DISTINCT (CAST(a.`imsi` AS CHAR)) AS 'imsi' "
         ",a.`iccid` AS 'iccid' "
@@ -131,72 +136,72 @@ def fetchUpdateDataFromSys(sql_info,update_imsi):
         "a.`imsi` IN  " + "(" + update_imsi + ")"
         "GROUP BY (CAST(a.`imsi` AS CHAR)) "
     )
-    N_Data=qureResultAsJson(sysStr=sql_info['getUpdateData']['db'],
-                            Database=sql_info['getUpdateData']['database'],
-                            query_str=N_DataStr,
-                            where=[])
+    N_Data = qureResultAsJson(sysStr=sql_info['getUpdateData']['db'],
+                              Database=sql_info['getUpdateData']['database'],
+                              query_str=N_DataStr,
+                              where=[])
     if N_Data:
         N_returnData = add_dickey(Json_Result=N_Data, key_value={'bu_group': 'N'})
-        update_data=[]
+        update_data = []
         update_data.extend(N_returnData)
-        update_data=JsonDataConvert(Json_Result=update_data)
+        update_data = JsonDataConvert(Json_Result=update_data)
         return update_data
     else:
         return False
 
 
-def mergeData(mergeKy,baseData,mergedData):
-    """
-
+def mergeData(mergeKy, baseData, mergedData):
+    """=========================================
+    :param mergeKy:
     :param baseData:
     :param mergedData:
     :return:
-    """
-    item=mergeKy
+    ==========================================="""
+    item = mergeKy
     for md in mergedData:
         for bd in baseData:
-            if (str(md[item]) == str(bd[item])):
+            if str(md[item]) == str(bd[item]):
                 bd.update(md)
     return baseData
 
 
 def popBlankValue(popData):
-    """
-
-    :param popData:
+    """========================================================
+    该函数剔除 popData 中的所有{key: value} 中 value为空字符("")
+    使插入数据的每一行都为有效数据
+    :param popData: 待处理数据[{},{},{}...]
     :return:
-    """
-    if popData==[]:
+    ============================================================"""
+    if not popData:
         return False
     for i in range(len(popData)):
-        popDic=[]
-        for dic in popData[i].keys():
-            if popData[i][dic]=="":
-                popDic.append(dic)
-        for dic in popDic:
+        popDic = []
+        for dic in popData[i].keys():   # 获取第i个字典的key列表
+            if popData[i][dic] == "":   # 判断是否为空字符段
+                popDic.append(dic)      # 记录对应key
+        for dic in popDic:              # pop 出第i 个数据中的空字段
             popData[i].pop(dic)
     return popData
 
-def insertStrMake(databaseTable,insertData):
-    """
+
+def insertStrMake(databaseTable, insertData):
+    """======================================
     完成插入SQL语句组合
     databaseTable:插入数据库表单名
     insertData：更新数据
-    """
+    =========================================="""
     if 'imsi' not in insertData.keys():
-            insertStrInfo = {"err": True,"errInfo":"存在无IMSI主键行！", "insertStr": ""}
+            insertStrInfo = {"err": True, "errInfo": "存在无IMSI主键行！", "insertStr": ""}
             return insertStrInfo
     else:
         dicList = list(insertData.keys())
         dicList.remove('imsi')
-        index = "imsi"#插入主键定义
+        index = "imsi"                                              # 插入主键定义
         insertHeadStr = "INSERT INTO  `" + databaseTable + "`  "
         insertColumStr = ""
         insertValueStr = ""
-        value = []
-
         for j in range(len(dicList)):
-            if (j==len(dicList) - 1):
+            if j == (len(dicList) - 1):
                 Columtemp = dicList[j] + " "
                 Valuetemp = "%(" + dicList[j] + ")s "
             else:
@@ -212,12 +217,12 @@ def insertStrMake(databaseTable,insertData):
 
 
 def insertOneColModel(sql_info, insertData):
-    """
+    """========================================
 
     :param sql_info:
     :param insertData:
     :return:
-    """
+    ============================================"""
     errInfo = ''
     try:
         cnx = get_DataBaseConn(sysSql=sql_info['insertDB']['db'], Database=sql_info['insertDB']['database'])
@@ -235,7 +240,7 @@ def insertOneColModel(sql_info, insertData):
                 insert_value = insertData[i]
                 insert_section = insertAction["insertStr"]
                 cursor.execute(insert_section, insert_value)
-        commit = cnx.commit()
+        cnx.commit()
         cursor.close()
         cnx.close()
     except mysql.connector.Error as err:
@@ -245,12 +250,12 @@ def insertOneColModel(sql_info, insertData):
 
 
 def insertModel(SqlInfo, DicData):
-    """
+    """==========================================
 
     :param SqlInfo:
     :param DicData:
     :return:
-    """
+    ============================================="""
     inser_data = DicData
     sql_set = SqlInfo
     if inser_data:
