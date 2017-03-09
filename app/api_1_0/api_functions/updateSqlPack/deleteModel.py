0778cc50f30ffbc944448d4b8a89265de3e3e30a
kissf <chois_lu@outlook.com>
2017-03-09 09:51:57 +0800

    update insertmodel of api_0_1

--- app/api_1_0/api_functions/updateSqlPack/deleteModel.py
+++ app/api_1_0/api_functions/updateSqlPack/deleteModel.py
@@ -30,24 +30,28 @@ def popBlankValue(popData):
     return popData
 
 
-def deleteManuleVsim(SqlInfo, DicData):
-    """
+def deleteSqlData(sql_info, DicData, key):
+    """============================
 
-    :param SqlInfo:
-    :param DicData:
-    :return:
-    """
-    errInfo = ''
+    :param sql_info: 数据库连接信息 {"db": "config_DevAmz", "database": "devdatabase", "sheet": "vsim_test_info"}
+    :param DicData: 待删除数据，为主键信息,剔除空白数据后的数据
+    :param key: 主键名称
+    =================================
+    :return: errInfo 数据库删除数据后信息
+    =========================================="""
+    SqlInfo = sql_info
     imsiData = DicData
+    deleteKey = key
+    errInfo = ''
     strimsi = ""
     for i in range(len(imsiData)):
         if i != (len(imsiData) - 1):
-            strimsi = strimsi + "'" + imsiData[i]['imsi'] + "',"
+            strimsi = strimsi + "'" + imsiData[i][deleteKey] + "',"
         else:
-            strimsi = strimsi + "'" + imsiData[i]['imsi'] + "'"
+            strimsi = strimsi + "'" + imsiData[i][deleteKey] + "'"
     deleSection = (
         "DELETE FROM `" + SqlInfo["sheet"] + "` "
-        "WHERE `imsi` IN  (" + strimsi + ")"
+        "WHERE `" + deleteKey + "` IN  (" + strimsi + ")"
     )
     try:
         cnx = get_DataBaseConn(sysSql=SqlInfo['db'],
@@ -60,20 +64,21 @@ def deleteManuleVsim(SqlInfo, DicData):
     except mysql.connector.Error as err:
         errInfo = "Something went wrong: {}".format(err)
         return errInfo
+
     return errInfo
 
 
-def deleteManualModel(SqlInfo, arrayDicData):
-    """
+def deleteModel(SqlInfo, arrayDicData, delete_key):
+    """========================================
 
-    :param SqlInfo:
-    :param arrayDicData:
-    :return:
-    """
+    :param SqlInfo: 删除数据库信息 {"db": "config_DevAmz", "database": "devdatabase", "sheet": "vsim_test_info"}
+    :param arrayDicData: 原始数据 [{},{},...]
+    :return: 返回操作信息，为字符串 string
+    =========================================="""
     deletData = arrayDicData
     if deletData:
         popdata = popBlankValue(deletData)
-        DeleteActionInfo = deleteManuleVsim(SqlInfo=SqlInfo, DicData=popdata)
+        DeleteActionInfo = deleteSqlData(sql_info=SqlInfo, DicData=popdata, key=delete_key)
 
         return DeleteActionInfo
     else:
